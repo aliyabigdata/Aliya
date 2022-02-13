b97add633a15aab9596a6b01b08b2f5b0da494a4
Aliya's folder <aliya.bermaganbet@alumni.nu.edu.kz>
2022-02-13 14:23:17 +0300

    The final commits are done before piscine

--- aliya.c
+++ aliya.c
@@ -4,6 +4,8 @@ int main()
  printf("I created my final folder\n");
  printf("I created my folder\n");
  printf("The final folder is created\n");
+ printf("The final folder is created and adited by me\n");
+ printf("So it was the final folder before piscine\n");
  return 0;
 }
 
