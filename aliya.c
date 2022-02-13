fd6dfcef2ce62d0bb9766d9023360ed9b73369e5
Aliya's folder <aliya.bermaganbet@alumni.nu.edu.kz>
2022-02-13 14:19:43 +0300

    I used / symbol

--- aliya.c
+++ aliya.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main()
 {
- printf("I created my final folder");
+ printf("I created my final folder\n");
+ printf("I created my folder\n");
+ printf("The final folder is created\n");
  return 0;
 }
 
